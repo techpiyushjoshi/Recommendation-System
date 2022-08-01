cb00005293d05ab7863675ea36746c2faf2a4df8
Piyush Joshi <101545064+techpiyushjoshi@users.noreply.github.com>
2022-08-01 14:59:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+# We can use Flask for deploying also but we will use streamlit here
+# its easy with it
+
+# While using new library open documenation
+
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+
+# to hit api we need to install request
+
+def fetch_poster(movie_id):
+    response = requests.get(
+        'https://api.themoviedb.org/3/movie/{}?api_key=a91f8a4fbf641681be3d3fff31afafca&language=en-US'.format(
+            movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+    # to get the complete path we need to add
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].id
+        # fetch poster from API
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
+
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.title('Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+    'Which type of movies you would like to watch Kuku?',
+    movies['title'].values)
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+
+# in the dropdown we have to show the list of movies
+# which is in the jupyter botebook file, for which we will use pickle library
